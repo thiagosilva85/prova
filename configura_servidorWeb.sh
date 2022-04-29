6ac53b528a0228ea3c8a3d48bc610ec15ac42314
Thiago Silva <thiago.silva@iesb.edu.br>
2022-04-28 21:20:43 -0300

    Nova versão do script

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -6,4 +6,19 @@ apt-get install apache2 -y
 
 apt-get install wget -y
 
+wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
 
+apt-get install zip -y
+apt-get install unzip -y
+
+unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+cp p1/873219-690e1f604dc8ead2583589e1aee6f8a42040a035/cv-resumido.html /var/www/html/cv-resumido.html
+
+cp p1/873219-690e1f604dc8ead2583589e1aee6f8a42040a035/style.css /var/www/html/style.css
+
+rm /var/www/html/index.html
+
+mv /var/www/html/cv-resumido.html  /var/www/html/index.html
+
+/etc/init.d/apache2 start
